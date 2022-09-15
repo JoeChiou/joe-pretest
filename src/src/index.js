0f4d3774c364912b2ad011d04ab05008c188299e
JoeChouHearts <79893211+JoeChouHearts@users.noreply.github.com>
2022-09-16 00:31:53 +0800

    component OK

---
+++ src/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CustomInputNumber } from './component/custom-number-input';
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/*' element={<CustomInputNumber onChange={(e)=>{}} onBlur={(e)=>{}} name={'CustomInputNumber'}/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>);
+
