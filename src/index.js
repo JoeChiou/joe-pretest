e1755003280127afbfac79528fc0e688c3644e11
JoeChiou <iamasheep2008@gmail.com>
2022-09-16 10:38:26 +0800

    folder fixed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { CustomInputNumber } from './component/custom-number-input';
+import { CustomInputNumber } from './component';
+import { RoomAllocation } from './pages';
 
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/*' element={<CustomInputNumber onChange={(e)=>{}} onBlur={(e)=>{}} name={'CustomInputNumber'}/>} />
+        <Route path='/*' element={<CustomInputNumber onChange={(e) => { }} onBlur={(e) => { }} name={'CustomInputNumber'} />} />
+        <Route path='/room-allocation' element={<RoomAllocation />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/page-room-allocation/index.js
@@ -0,0 +1,6 @@
+export const RoomAllocation = () => {
+
+  return (
+    <>12312445room</>
+  )
+}
